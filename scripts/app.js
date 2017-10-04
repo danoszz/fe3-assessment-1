06f33580043fd3f1ba0b058ecf05fb40c0487a62
danoszz <danoszz@gmail.com>
2017-10-04 15:17:35 +0200

    ES5 to ES6 edits  & finnaly some comments 👀

--- scripts/app.js
+++ scripts/app.js
@@ -1,48 +1,53 @@
+const width = 800;
+const height = 800;
 
-var svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
+const svg = d3
+	.select('body')
+	.append('svg')
+	.attr('width', width)
+	.attr('height', height)
+	.append('g')
 
-var format = d3.format(",d");
+const format = d3.format(",d");
 
-var color = d3.scaleOrdinal(d3.schemeCategory20c);
+const color = d3.scaleOrdinal(d3.schemeCategory20c);
 
-var pack = d3.pack()
+const pack = d3.pack()
     .size([width, height])
     .padding(1.5);
 
-d3.tsv("https://danoszz.github.io/fe3-assessment-1/assets/data/languages.tsv", function(d) {
+d3.tsv("https://danoszz.github.io/fe3-assessment-1/assets/data/languages.tsv", d => {
   d.speakers = +d.speakers;
   if (d.speakers) return d;
-}, function(error, classes) {
-  if (error) throw error;
+}, (e, c) => {
+  if (e) throw e;
 
-  var root = d3.hierarchy({children: classes})
-      .sum(function(d) { return d.speakers; })
-      .each(function(d) {
+  const root = d3.hierarchy({children: c})
+      .sum(d => { return d.speakers; })
+      .each(d => {
         if (language = d.data.language) {
-          var language, i = language.lastIndexOf(".");
+          language, i = language.lastIndexOf(".");
           d.language = language;
           d.package = language.slice(0, i);
           d.class = language.slice(i + 1);
         }
       });
 
-  var node = svg.selectAll(".node")
+  let node = svg.selectAll(".node")
     .data(pack(root).leaves())
     .enter().append("g")
       .attr("class", "node")
-      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+      .attr("transform", d =>{ return "translate(" + d.x + "," + d.y + ")"; });
 
   node.append("circle")
-      .attr("language", function(d) { return d.language; })
+      .attr("language", d => { return d.language; })
       .attr("r", function(d) { return d.r; })
       .style("fill", function(d) { return color(d.package); });
 
   node.append("text")
-    .text(function (d) {  return d.language})
+    .text(d =>{  return d.language})
     .attr("text-anchor", "middle");
 
   node.append("title")
-      .text(function(d) { return d.language + "\n" + format(d.speakers); });
+      .text(d => { return d.language + "\n" + format(d.speakers); });
 });
